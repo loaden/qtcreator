073d3395b1e2f944f4fc4e8b183df4b99c89e4b8
Yuchen Deng <loaden@gmail.com>
2012-09-25 11:09:48 +0800

    QtHelper: Fix typo + add qt_wrap_cpp macro
    Change-Id: Ia91644f685ecc3ef80bdad4a9d385445162e3f88

--- cmake/QtHelper.cmake
+++ cmake/QtHelper.cmake
@@ -8,10 +8,14 @@ macro(qt_use_modules target)
     qt5_use_modules(${target} ${ARGN})
 endmacro()
 
-macro(qt_wrap_ui target)
-    qt5_wrap_ui(${target} ${ARGN})
+macro(qt_wrap_cpp outfiles)
+    qt5_wrap_cpp(${outfiles} ${ARGN})
 endmacro()
 
-macro(qt_add_resources target)
-    qt5_add_resources(${target} ${ARGN})
+macro(qt_wrap_ui outfiles)
+    qt5_wrap_ui(${outfiles} ${ARGN})
+endmacro()
+
+macro(qt_add_resources outfiles)
+    qt5_add_resources(${outfiles} ${ARGN})
 endmacro()
