74c3d053e53060b99461ec4f600bd2474ae3adaa
Yuchen Deng <loaden@gmail.com>
2012-09-26 16:11:53 +0800

    Have to disable ExtensionSystem's PCH support when using GCC
    ExtensionSystem_pch.h.gch: not used because `IDE_TEST_DIR'
    defined as ` "."' not ` .'
    
    Change-Id: I25261bd7eb82753768f08f9cf245aa0fd89d9745

--- cmake/QtcConfig.cmake
+++ cmake/QtcConfig.cmake
@@ -6,6 +6,10 @@ elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     set(MACOSX TRUE)
 endif()
 
+if(CMAKE_COMPILER_IS_GNUC OR CMAKE_COMPILER_IS_GNUCXX)
+    set(GCC TRUE)
+endif()
+
 set(QTC_PCH_SUPPORT ON
     CACHE BOOL "Precompiled Header support"
 )
