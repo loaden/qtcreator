b905a765570c618c43c3697bc9ce464c41dd39fb
Yuchen Deng <loaden@gmail.com>
2012-10-02 13:37:50 +0800

    Auto generate json file for plugins
    Change-Id: I0b1912fa1eb7356907033f5b5991aabd0de833e8

--- cmake/QtcPlugin.cmake
+++ cmake/QtcPlugin.cmake
@@ -3,6 +3,7 @@ set(QTC_PLUGIN_PATH lib/qtcreator/plugins/Nokia)
 macro(qtc_add_plugin target)
     add_library(${target} SHARED ${ARGN} ${pchSrcVar})
     qtc_generate_pluginspec(${target})
+    qtc_generate_json(${target})
     set_target_properties(${target} PROPERTIES
         LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/${QTC_PLUGIN_PATH}
         RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/${QTC_PLUGIN_PATH}
@@ -33,4 +34,16 @@ function(qtc_generate_pluginspec target)
     install(FILES ${specFile}
         DESTINATION ${QTC_PLUGIN_PATH}
     )
+endfunction()
+
+function(qtc_generate_json target)
+    if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${target}.json)
+       return()
+    endif()
+    set(specFile ${CMAKE_BINARY_DIR}/${QTC_PLUGIN_PATH}/${target}.pluginspec)
+    set(jsonFile ${CMAKE_CURRENT_BINARY_DIR}/${target}.json)
+    if(${specFile} IS_NEWER_THAN ${jsonFile})
+        # FIXME: Read data from pluginspec file
+        file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/${target}.json "{}")
+    endif()
 endfunction()
