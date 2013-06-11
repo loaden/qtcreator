aedfd8637eabb506e3be1203664faac37cac2e6e
Yuchen Deng <loaden@gmail.com>
2013-06-11 22:11:38 +0800

    Change the default editor fonts
    Change-Id: Ib7c19756cd125ae403cb755778a30e256affe4ca

--- src/plugins/texteditor/fontsettings.cpp
+++ src/plugins/texteditor/fontsettings.cpp
@@ -411,8 +411,8 @@ static QString defaultFontFamily()
     if (Utils::HostOsInfo::isMacHost())
         return QLatin1String("Monaco");
     if (Utils::HostOsInfo::isAnyUnixHost())
-        return QLatin1String("Monospace");
-    return QLatin1String("Courier");
+        return QLatin1String("Monaco");
+    return QLatin1String("Consolas");
 }
 
 QString FontSettings::defaultFixedFontFamily()
@@ -431,8 +431,8 @@ int FontSettings::defaultFontSize()
     if (Utils::HostOsInfo::isMacHost())
         return 12;
     if (Utils::HostOsInfo::isAnyUnixHost())
-        return 9;
-    return 10;
+        return 11;
+    return 11;
 }
 
 /**
